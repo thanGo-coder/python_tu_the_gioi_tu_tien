509b126ba100621d8dae88a1155f2b8fd3f69ea7
thango_coder <trananhtuan09042014@gmail.com>
2025-09-26 19:45:51 +0700

    toi oder do an

--- baitapvenha/_1_so.py
+++ baitapvenha/_1_so.py
@@ -11,8 +11,16 @@
 # - **Output**:
     
 #     `"Số lẻ"` hoặc `"Số chẵn"`
-so=int(input("nhapso     "))
-if so%2==0:
-    print("sochan")
+so = int(input("Nhập số: "))
+if so == 0:
+    print("Số 0 là số chẵn và không âm không dương.")
+elif so > 0:
+    if so % 2 == 0:
+        print(f"Số {so} là số chẵn và là số dương.")
+    else
+        print(f"Số {so} là số lẻ và là số dương.")
 else:
-    print("sole")
+    if so % 2 == 0:
+        print(f"Số {so} là số chẵn và là số âm.")
+    else:
+        print(f"Số {so} là số lẻ và là số âm.")
